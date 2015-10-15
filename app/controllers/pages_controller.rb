8a640acd4c8704ec8a5c6415788040d2a3ec4f95
Andy Mitchell <themitchell@me.com>
2015-10-15 22:28:23 +0100

    Move each section to its own page - styling is now broken

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def what_we_represent
+  end
+
+  def case_studies
+  end
+
+  def testimonials
+  end
+
+  def environmental_policy
+  end
+
+  def services
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'case-studies', to: 'pages#case_studies'
+  get 'contact', to: 'pages#contact'
+  get 'environmental-policy', to: 'pages#environmental_policy'
+  get 'services', to: 'pages#services'
+  get 'testimonials', to: 'pages#testimonials'
+  get 'what-we-represent', to: 'pages#what_we_represent'
+
   root to: 'pages#index'
 end
