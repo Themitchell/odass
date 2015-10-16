6d2c8346d5ad1df296fc2c29a1438e2c4b8c0d19
Andy Mitchell <themitchell@me.com>
2015-10-16 23:45:27 +0100

    Highlight current nav item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_link_to(link_text, link_path, options={})
+    options[:class] = current_page?(link_path) ? "current #{options[:class]}" : options[:class]
+
+    link_to link_text, link_path, options
+  end
 end
