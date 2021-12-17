cb013138af3d7f944221797157c0ad1ca56a6cd2
Andy Mitchell <326561+Themitchell@users.noreply.github.com>
2021-12-17 16:39:22 +0000

    Shutdown business

--- app/helpers/application_helper.rb
+++
@@ -1,7 +0,0 @@
-module ApplicationHelper
-  def nav_link_to(link_text, link_path, options={})
-    options[:class] = current_page?(link_path) ? "current #{options[:class]}" : options[:class]
-
-    link_to link_text, link_path, options
-  end
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'case-studies', to: 'pages#case_studies'
-  get 'contact', to: 'pages#contact'
-  get 'environmental-policy', to: 'pages#environmental_policy'
-  get 'services', to: 'pages#services'
-  get 'testimonials', to: 'pages#testimonials'
-
-  root to: 'pages#index'
-end
